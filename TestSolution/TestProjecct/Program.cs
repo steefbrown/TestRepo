daf0aa7b108fac2336625d073de51f97d1f08e40
steefbrown <steefbrown@gmail.com>
2015-01-21 08:51:33 -0700

    Test solution added

---
+++ TestSolution/TestProjecct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjecct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a test
+        }
+    }
+}
